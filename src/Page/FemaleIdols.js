a538b7f2566042ae8ab4aaba6f760be345fb5766
Soojin Park <soozin95@gmail.com>
2022-03-04 17:27:39 -0800

    Add files via upload

---
+++ src/Page/FemaleIdols.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import classes from "./FemaleIdols.module.css";
+import FemaleIdolsData from "../Data/FemaleIdolsData";
+
+const FemaleIdols = () => {
+  const [idols, setIdols] = useState([]);
+  const [displays, setDisplays] = useState([]);
+  const [winners, setWinners] = useState([]);
+
+  useEffect(() => {
+    FemaleIdolsData.sort(() => Math.random() - 0.5);
+    setIdols(FemaleIdolsData);
+    setDisplays([FemaleIdolsData[0], FemaleIdolsData[1]]);
+  }, []);
+
+  const clickHandler = (idol) => () => {
+    if (idols.length <= 2) {
+      if (winners.length === 0) {
+        setDisplays([idol]);
+      } else {
+        let updatedIdol = [...winners, idol];
+        setIdols(updatedIdol);
+        setDisplays([updatedIdol[0], updatedIdol[1]]);
+        setWinners([]);
+      }
+    } else if (idols.length > 2) {
+      setWinners([...winners, idol]);
+      setDisplays([idols[2], idols[3]]);
+      setIdols(idols.slice(2));
+    }
+  };
+
+  return (
+    <div className={classes.body}>
+      <h1 className={classes.title}>Who is your type?</h1>
+      <a href="/" className={classes.goBackBtn}>
+        Go back
+      </a>
+      {displays.map((e) => (
+        <div className={classes.flex1} key={e.name}>
+          <img src={e.image} onClick={clickHandler(e)}></img>
+          <div className={classes.name}>
+            {e.name} ({e.occupation})
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FemaleIdols;
---
+++ src/Page/MaleFastMode.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import classes from "./MaleFastMode.module.css";
+import array from "../Data/MaleIdolsData";
+
+const MaleFastMode = () => {
+  const [displays, setDisplays] = useState([]);
+  const [filteredArr, setFilteredArr] = useState([]);
+
+  useEffect(() => {
+    let arr = array.sort(() => Math.random() - 0.5);
+    setDisplays([arr[0], arr[1]]);
+    setFilteredArr(arr);
+  }, [setDisplays]);
+
+  const clickHandler = (e) => () => {
+    displays.forEach(function (v, i, d) {
+      const tags = e.tags;
+      let filtered = filteredArr.filter((e) => {
+        const arrayTags = e.tags;
+        return tags.some((f) => arrayTags.includes(f));
+      });
+
+      if (v.id === e.id) {
+        if (filteredArr.length > 2) {
+          setFilteredArr(filtered);
+
+          filtered.push(filteredArr[i]);
+          filtered.splice(i, 1);
+          displays.filter((displays) => displays !== e);
+          filtered.splice(d, 1);
+          setDisplays([filtered[0], filtered[1]]);
+          console.log(filteredArr[i].tags);
+        } else if (filteredArr.length <= 2) {
+          setDisplays([filtered[i]]);
+        }
+      }
+    });
+  };
+
+  return (
+    <div className={classes.body}>
+      <h1 className={classes.title}>Who is your type?</h1>
+      <a href="/" className={classes.goBackBtn}>
+        Go back
+      </a>
+      {displays.map((e, index) => (
+        <div className={classes.flex1} key={e.name}>
+          <img src={e.image} onClick={clickHandler(e)}></img>
+          <div className={classes.name}>
+            {e.name} ({e.occupation})
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MaleFastMode;
